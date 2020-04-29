69cbfd4664ad4b8893369df6e2549bc26fa55ec8
magnarok49 <magneas49@hotmail.com>
2020-04-29 13:46:45 +0200

    fixed ignore, cleaned up code for part2task2

--- task2/kmeans_nearest_neighbour.m
+++ task2/kmeans_nearest_neighbour.m
@@ -1,5 +1,6 @@
 %% Part 2, Task 2
 clc; clear all; close all;
+
 % Initialization
 if ~exist('num_test','var')
     load("MNist_ttt4275/data_all.mat");
@@ -10,70 +11,59 @@ N = 784; % number of features per vector
 I = 10;  % number of classes
 
 %% Task 2a: Clustering training vectors
+
 idx = cell(10,1);
 C   = cell(10,1);
 
+tic
 for i = 1:I
     [idx{i}, C{i}] = kmeans(trainv((trainlab+1) == i,:),M);
 end
+T_cluster = toc;
 
 %% Task 2b: Running NN classification using clusters
 
 % this one looks at only the best fit for each number
 % then chooses the best fit from each number
 % should be subject to outliers where a single cluster has an exceptional
-% fit for one number, but the average fit is much better for another class
+% fit for one class, but the average fit is much better for another class
 
 tic
-best_dist_class      = zeros(I, num_test);
-results_dist         = zeros(1,num_test);
-results_class_offs   = zeros(1,num_test);
+best_dist_class    = zeros(I, num_test);
+results_dist_      = zeros(1,num_test);
+results_class_offs = zeros(1,num_test);
 
 for i = 1:I
     best_dist_class(i,:) = min(dist(C{i}, testv'));
 end
 
-[results_dist, results_class_offs] = min(best_dist_class);
+[results_dist_, results_class_offs] = min(best_dist_class);
 results = results_class_offs' - 1;
 
-confmat  = confusionmat(testlab, results)
-err_rate = (num_test - trace(confmat))/num_test
-toc
+NN_cluster_confmat  = confusionmat(testlab, results);
+NN_cluster_err_rate = (num_test - trace(NN_cluster_confmat))/num_test;
+T_NN_cluster        = toc;
 
 %% Task 2c: Running KNN, K=7
-% similar to above task, do majority vote among 7 best of all distances
 
-% this one looks at the 7 best fits from kmeans overall
-% then does a majority vote among these
-% not sure what to call it, but this one definitely does too much fitting
-% kmeans should already have smoothed out outliers from training set.
+% This one looks at the 7 best fits from all kmeans clusters
+% then does a majority vote among these. I'm not sure what to call it, 
+% but this one definitely does too much smoothing.
+% Kmeans should already have smoothed out outliers from training set.
 % thus, doing a majority vote on these smoothed templates means that you
 % end up with a "twice smoothed" decision.
 
-% this is probably useful for cases where the variance within the classes
-% is much bigger and the kmeans templates are liable to overlap between
-% classes
+% Peformance here is slightly worse than the NN_cluster variety used in
+% task 2b: roughly 1% more error, and is usually half a second faster. 
 
-% might also be more useful as the number of clusters per class increases
-% as this increases the likehood of outliers that coincidentally have the 
+% This method probably useful for cases where the variance 
+% within each class is much bigger and the kmeans clusters
+% are liable to overlap between classes.
 
-tic
-K = 7;
+% Might also be more useful as the number of clusters per class
+% increases as this increases the likelihood of outliers that 
+% coincidentally have the shortest distance. 
 
-all_distances = zeros(I*M,num_test);
-
-for i = 1:I
-    all_distances(1 + M*(i-1):M*i,:) = dist(C{i}, testv');
-end
-
-[B_, I_offs] = mink(all_distances, 7);
-results = mode(idivide(uint16(I_offs - 1),64))';
-
-KNN_confmat  = confusionmat(uint16(testlab), results)
-KNN_err_rate = (num_test - trace(KNN_confmat))/num_test
-toc
-
-%% Task2c: again with proper dispute handling
 tic
 K = 7;
 
@@ -87,45 +77,32 @@ end
 Votes = idivide(uint16(I_offs - 1),64);
 [Modes, Freqs] = mode(Votes);
 
- % Look for conflicts where mode has freq <= 3
+% ---------- RESOLVING MODE CONFLICTS -----------
+% Look for conflicts where mode has 3 or less votes
 for i = find(Freqs <= 3)
-    
-    candidates = zeros(1,1);
-    votes = Votes(:,i);
-    j = 1;
-    
-    % fill candidates vector with all classes 
-    % that have the same freq as inital mode
-    while 1
-        [m, f] = mode(votes);
-        if f ~= Freqs(i)
-            break;
+    % count occurrences of all votes 
+    [votecounts, edges_] = histcounts(Votes(:,i), -0.5:1:9.5);
+    % if the majority vote is contested
+    if size(find(votecounts == Freqs(i)),2)
+        %find closest neighbour which is also a candidate
+        for j = 1:K
+            best_candidate = Votes(j,i);
+            if votecounts(best_candidate + 1) == Freqs(i)
+                break;
+            end    
         end
-        candidates(j) = m;
-        for k = find(votes == m)
-            votes(k) = 10 + k;
-        end
-        j = j + 1;
+        Modes(1,i) = best_candidate;
     end
-    
-    % for only one candidate, go to next potential conflict
-    if size(candidates, 2) == 1
-        continue;
-    end
-    
-    % find closest neighbour which is also a candidate
-    for j = 1:K
-        best_candidate = idivide(uint16(I_offs(j,i) - 1),64);
-        if nnz(candidates == best_candidate) > 0 
-            break;
-        end    
-    end
-    
-    % set decision as closest neighbour which is also a candidate
-    Modes(1,i) = best_candidate;
 end
 
 results = Modes';
-KNN_2_confmat  = confusionmat(uint16(testlab), results)
-KNN_2_err_rate = (num_test - trace(KNN_2_confmat))/num_test
-toc
+
+KNN_confmat   = confusionmat(uint16(testlab), results);
+KNN_err_rate  = (num_test - trace(KNN_confmat))/num_test;
+T_KNN_cluster = toc;
+
+%% Save results
+
+save("all_results_part2_task2.mat", "T_cluster", ...
+    "T_NN_cluster", "NN_cluster_confmat", "NN_cluster_err_rate", ...
+    "T_KNN_cluster", "KNN_confmat",  "KNN_err_rate");
